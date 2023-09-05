a0ded65d071cbcdc47edeabe42c02ea81329fcc9
holgermueller <9jholger@gmail.com>
2023-09-05 11:27:08 -0500

    updated year in footer, now updates automatically

--- src/App.vue
+++ src/App.vue
@@ -10,7 +10,7 @@
       <Dash />
     </v-main>
 
-    <footer>&copy; 2021-2023 Holger Mueller</footer>
+    <footer>&copy; 2021 - {{ new Date().getFullYear() }} Holger Mueller</footer>
   </v-app>
 </template>
 
